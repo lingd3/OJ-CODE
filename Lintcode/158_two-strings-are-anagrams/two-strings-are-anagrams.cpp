2f9f3bff8e557e855ae8730e080b0f8a25739b3b
linqiaonihao <824928207@qq.com>
2016-09-18 23:11:53 +0800

    code

---
+++ Lintcode/158_two-strings-are-anagrams/two-strings-are-anagrams.cpp
@@ -0,0 +1,28 @@
+/*
+@Copyright:LintCode
+@Author:   linqiao
+@Problem:  http://www.lintcode.com/problem/two-strings-are-anagrams
+@Language: C++
+@Datetime: 16-08-30 16:13
+*/
+
+class Solution {
+public:
+    /**
+     * @param s: The first string
+     * @param b: The second string
+     * @return true or false
+     */
+    bool anagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        int a[26] = {0};
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i]-'a' >= 0 && s[i]-'a' <= 25) a[s[i]-'a']++;
+            if (t[i]-'a' >= 0 && t[i]-'a' <= 25) a[t[i]-'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != 0) return false;
+        }
+        return true;
+    }
+};
---
+++ Lintcode/55_compare-strings/compare-strings.cpp
@@ -0,0 +1,30 @@
+/*
+@Copyright:LintCode
+@Author:   linqiao
+@Problem:  http://www.lintcode.com/problem/compare-strings
+@Language: C++
+@Datetime: 16-08-30 16:17
+*/
+
+class Solution {
+public:
+    /**
+     * @param A: A string includes Upper Case letters
+     * @param B: A string includes Upper Case letter
+     * @return:  if string A contains all of the characters in B return true 
+     *           else return false
+     */
+    bool compareStrings(string A, string B) {
+        int a[26] = {0};
+        for (int i = 0; i < A.size(); i++) {
+            a[A[i]-'A']++;
+        }
+        for (int i = 0; i < B.size(); i++) {
+            a[B[i]-'A']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (a[i] < 0) return false;
+        }
+        return true;
+    }
+};
