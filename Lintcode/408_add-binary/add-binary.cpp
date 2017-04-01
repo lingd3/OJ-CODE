6cfd1beb74c2053904fe67996dfaacc811268415
linqiaonihao <824928207@qq.com>
2017-04-01 21:35:23 +0800

    code

---
+++ Lintcode/408_add-binary/add-binary.cpp
@@ -0,0 +1,72 @@
+/*
+@Copyright:LintCode
+@Author:   linqiao
+@Problem:  http://www.lintcode.com/problem/add-binary
+@Language: C++
+@Datetime: 17-04-01 12:43
+*/
+
+class Solution {
+public:
+    /**
+     * @param a a number
+     * @param b a number
+     * @return the result
+     */
+    string addBinary(string& a, string& b) {
+        // Write your code here
+        int carry = 0, c = 0, i, j;
+        char sum[100] = {'\0'};
+        for (i = a.size()-1, j = b.size()-1; i >= 0 && j >= 0; i--, j--) {
+            if (a[i]-'0'+b[j]-'0'+carry > 1) {
+                sum[c] = a[i]-'0'+b[j]+carry-2;
+                c++;
+                carry = 1;
+            }
+            else {
+                sum[c] = a[i]-'0'+b[j]+carry;
+                c++;
+                carry = 0;
+            }
+        }
+        if (i < 0) {
+            for (; j >= 0; j--) {
+                if (b[j]-'0'+carry > 1) {
+                    sum[c] = b[j]+carry-2;
+                    c++;
+                    carry = 1;
+                }
+                else {
+                    sum[c] = b[j]+carry;
+                    c++;
+                    carry = 0;
+                }
+            }
+        }
+        else if (j < 0) {
+            for (; i >= 0; i--) {
+                if (a[i]-'0'+carry > 1) {
+                    sum[c] = a[i]+carry-2;
+                    c++;
+                    carry = 1;
+                }
+                else {
+                    sum[c] = a[i]+carry;
+                    c++;
+                    carry = 0;
+                }
+            }
+        }
+        if (carry == 1) {
+            sum[c] = '1';
+            c++;
+        }
+        char sum2[100] = {'\0'};
+        int d = 0;
+        for (c = c-1; c >= 0; c--) {
+            sum2[d] = sum[c];
+            d++;
+        }
+        return string(sum2);
+    }
+};
---
+++ Lintcode/569_add-digits/add-digits.cpp
@@ -0,0 +1,20 @@
+/*
+@Copyright:LintCode
+@Author:   linqiao
+@Problem:  http://www.lintcode.com/problem/add-digits
+@Language: C++
+@Datetime: 17-04-01 12:53
+*/
+
+class Solution {
+public:
+    /**
+     * @param num a non-negative integer
+     * @return one digit
+     */
+    int addDigits(int num) {
+        // Write your code here
+        return (num - 1) % 9 + 1;
+    }
+    
+};
